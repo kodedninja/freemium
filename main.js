d87e8abe5de7152c5d6c53eeca6ad06dd6177104
Hunor Karamán <hunix94@gmail.com>
2017-12-19 12:17:44 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var targetNode = document.body;
+var config = { attributes: true, childList: true };
+
+var callback = function(mutationsList) {
+	filter(document.querySelectorAll('.heroGrid-largeCard'));
+	filter(document.querySelectorAll('.heroGrid-smallCard'));
+	filter(document.querySelectorAll('.heroGrid-compact'));
+	filter(document.querySelectorAll('.u-size6of12'));
+	filter(document.querySelectorAll('.u-size4of12'));
+};
+
+var observer = new MutationObserver(callback);
+
+observer.observe(targetNode, config);
+
+function filter(elements) {
+	for (var i = 0; i < elements.length; i++) {
+		var icon = elements[i].querySelector('.svgIcon--star');
+		if (icon) elements[i].parentNode.removeChild(elements[i]);
+	}
+}
