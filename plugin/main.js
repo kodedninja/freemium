ee5f61c197c7153b2ffadef333e24659d57f9181
Hunor Karamán <hunix94@gmail.com>
2017-12-21 18:34:10 +0200

    removed seen class

--- plugin/main.js
+++ plugin/main.js
@@ -2,12 +2,12 @@ var targetNode = document.body;
 var config = { attributes: true, childList: true };
 
 var callback = function() {
-	filter(document.querySelectorAll('.heroGrid-largeCard:not(.freemiumseen)'));
-	filter(document.querySelectorAll('.heroGrid-mediumCard:not(.freemiumseen)'));
-	filter(document.querySelectorAll('.heroGrid-smallCard:not(.freemiumseen)'));
-	filter(document.querySelectorAll('.heroGrid-compact:not(.freemiumseen)'));
-	filter(document.querySelectorAll('.u-size6of12:not(.freemiumseen)'));
-	filter(document.querySelectorAll('.u-size4of12:not(.freemiumseen)'));
+	filter(document.querySelectorAll('.heroGrid-largeCard'));
+	filter(document.querySelectorAll('.heroGrid-mediumCard'));
+	filter(document.querySelectorAll('.heroGrid-smallCard'));
+	filter(document.querySelectorAll('.heroGrid-compact'));
+	filter(document.querySelectorAll('.u-size6of12'));
+	filter(document.querySelectorAll('.u-size4of12'));
 };
 
 var observer = new MutationObserver(callback);
@@ -17,6 +17,5 @@ function filter(elements) {
 	for (var i = 0; i < elements.length; i++) {
 		var icon = elements[i].querySelector('.svgIcon--star');
 		if (icon) elements[i].parentNode.removeChild(elements[i]);
-		else elements[i].classList.push('freemiumseen');
 	}
 }
